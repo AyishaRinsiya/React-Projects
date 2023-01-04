db7846f4721ab78c7f9a9b890ecc19f6455b6067
Ayisha Rinsiya <69690683+AyishaRinsiya@users.noreply.github.com>
2023-01-04 20:26:12 +0530

    Add files via upload

---
+++ shopping-cart/src/products.js
@@ -0,0 +1,59 @@
+import product1 from'./assets/1.jpg';
+import product2 from'./assets/2.jpeg';
+import product3 from'./assets/3.png';
+import product4 from'./assets/4.jpeg';
+import product5 from'./assets/5.png';
+import product6 from'./assets/6.webp';
+import product7 from'./assets/7.png';
+import product8 from'./assets/8.png';
+
+export const PRODUCTS=[
+    {
+        id: 1,
+        productName:"Hoodies",
+        price:700.0,
+        productImage:product1,
+    },
+    {
+        id: 2,
+        productName:"Skirt",
+        price:500.0,
+        productImage:product2,
+    },
+    {
+        id: 3,
+        productName:"Crop top",
+        price:450.0,
+        productImage:product3,
+    },
+    {
+        id: 4,
+        productName:"Shirt",
+        price:350.0,
+        productImage:product4,
+    },
+    {
+        id: 5,
+        productName:"Denim Jacket",
+        price:999.0,
+        productImage:product5,
+    },
+    {
+        id: 6,
+        productName:"Shorts",
+        price:650.0,
+        productImage:product6,
+    },
+    {
+        id: 7,
+        productName:"Cap",
+        price:250.0,
+        productImage:product7,
+    },
+    {
+        id: 8,
+        productName:"Jumpsuit",
+        price:950.0,
+        productImage:product8,
+    }
+]
